fc513cb50e417cd76842de7651df75e97a805d1c
Runek00 <runek00@gmail.com>
2023-08-28 00:47:56 +0200

    I made it more Springy

--- src/main/java/com/runek/contacts/ContactController.java
+++ src/main/java/com/runek/contacts/ContactController.java
@@ -1,6 +1,5 @@
 package com.runek.contacts;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
@@ -11,10 +10,8 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Set;
+import java.util.HashSet;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 @Controller
 public class ContactController {
@@ -32,17 +29,19 @@ public class ContactController {
     }
 
     @GetMapping("/contacts")
-    String contacts(HttpServletRequest request, Model model) {
-        String q = request.getParameter("q");
-        q = q == null ? "" : q;
-        int page = Integer.parseInt(request.getParameterMap().getOrDefault("page", new String[]{"0"})[0]);
+    String contacts(
+            @RequestParam(defaultValue = "") String q,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestHeader(name = "HX-Trigger", required = false) String trigger,
+            Model model) {
+        int pageLength = 10;
         model.addAttribute("q", q);
         model.addAttribute("page", page);
         model.addAttribute(
                 "contacts",
-                contactRepository.findAllByName(q, 10, page * 10));
+                contactRepository.findAllByName(q, pageLength, page * pageLength));
         model.addAttribute("archiver", Archiver.get());
-        if (page == 0 && !"search".equals(request.getHeader("HX-Trigger"))) {
+        if (page == 0 && !"search".equals(trigger)) {
             return "index";
         } else {
             return "rows";
@@ -50,10 +49,8 @@ public class ContactController {
     }
 
     @DeleteMapping("/contacts")
-    String bulkDeleteContacts(HttpServletRequest request, Model model) {
-        String[] stringIds = request.getParameterValues("selected_contact_ids");
-        Set<Long> ids = Arrays.stream(stringIds).map(Long::parseLong).collect(Collectors.toSet());
-        contactRepository.deleteAllById(ids);
+    String bulkDeleteContacts(@RequestParam HashSet<Long> selectedContactIds, Model model) {
+        contactRepository.deleteAllById(selectedContactIds);
         model.addAttribute("contacts", contactRepository.findAll());
         model.addAttribute("archiver", Archiver.get());
         return "index";
@@ -84,15 +81,14 @@ public class ContactController {
     }
 
     @PostMapping("/contacts/new")
-    String newContactPost(HttpServletRequest request) {
-        Contact c = new Contact(
-                null,
-                request.getParameter("first_name"),
-                request.getParameter("last_name"),
-                request.getParameter("email"),
-                request.getParameter("phone"));
+    String newContactPost(
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam String email,
+            @RequestParam String phone) {
+        Contact c = new Contact(null, firstName, lastName, email, phone);
         try {
-            String validationErrors = validateEmailFinal(null, request.getParameter("email"));
+            String validationErrors = validateEmailFinal(null, email);
             if (!"".equals(validationErrors)) {
                 throw new IllegalArgumentException(validationErrors);
             }
@@ -113,15 +109,16 @@ public class ContactController {
     }
 
     @PostMapping("/contacts/{id}/edit")
-    String editContactPost(@PathVariable Long id, HttpServletRequest request, Model model) {
-        Contact c = new Contact(
-                id,
-                request.getParameter("first_name"),
-                request.getParameter("last_name"),
-                request.getParameter("email"),
-                request.getParameter("phone"));
+    String editContactPost(
+            @PathVariable Long id,
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam String email,
+            @RequestParam String phone,
+            Model model) {
+        Contact c = new Contact(id, firstName, lastName, email, phone);
         try {
-            String validationErrors = validateEmailFinal(id, request.getParameter("email"));
+            String validationErrors = validateEmailFinal(id, email);
             if (!"".equals(validationErrors)) {
                 throw new IllegalArgumentException(validationErrors);
             }
@@ -135,9 +132,11 @@ public class ContactController {
 
     @DeleteMapping("/contacts/{id}")
     @ResponseBody
-    Object deleteContactPost(@PathVariable Long id, HttpServletRequest request) {
+    Object deleteContactPost(
+            @PathVariable Long id,
+            @RequestHeader(name = "HX-Trigger", required = false) String trigger) {
         contactRepository.deleteById(id);
-        if ("delete-btn".equals(request.getHeader("HX-Trigger"))) {
+        if ("delete-btn".equals(trigger)) {
             RedirectView rView = new RedirectView("/contacts");
             rView.setStatusCode(HttpStatusCode.valueOf(303));
             return rView;
@@ -189,7 +188,7 @@ public class ContactController {
     }
 
     @GetMapping("/contacts/archive/file")
-    void archiveFile(Model model, HttpServletResponse response) throws IOException {
+    void archiveFile(HttpServletResponse response) throws IOException {
         Archiver archiver = Archiver.get();
         response.setContentType("text/plain");
         response.setHeader("Content-Disposition", "attachment; filename=file.txt");
@@ -200,7 +199,7 @@ public class ContactController {
         String error = "";
         String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
                 + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-        if (Pattern.compile(regexPattern).matcher(email).matches()) {
+        if (!Pattern.compile(regexPattern).matcher(email).matches()) {
             error += "Wrong email pattern!\n";
         }
         boolean repeats = contactRepository.findAll()
--- src/main/java/com/runek/contacts/ContactsApplication.java
+++ src/main/java/com/runek/contacts/ContactsApplication.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @SpringBootApplication
 public class ContactsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ContactsApplication.class, args);
     }
